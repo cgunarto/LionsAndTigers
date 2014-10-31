f1495a11d8d1763203fe8140167b667c9fad55c3
Alexey Emelyanov <emelyanoff@gmail.com>
2014-10-31 12:50:37 -0700

    added ColleCtion View, custom cell and image View

---
+++ LionsAndTigers/CustomCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.h
+//  LionsAndTigers
+//
+//  Created by Alex on 10/31/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *cellImageView;
+
+@end
---
+++ LionsAndTigers/CustomCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.m
+//  LionsAndTigers
+//
+//  Created by Alex on 10/31/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import "CustomCollectionViewCell.h"
+
+@implementation CustomCollectionViewCell
+
+@end
