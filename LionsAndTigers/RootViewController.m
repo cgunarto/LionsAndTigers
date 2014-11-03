dc9eb94b1c91bac780e571aad276549f905728a5
Alexey Emelyanov <emelyanoff@gmail.com>
2014-11-03 08:56:03 -0800

    latest version

--- LionsAndTigers/RootViewController.m
+++ LionsAndTigers/RootViewController.m
@@ -10,7 +10,7 @@
 #import "PhotosViewController.h"
 #import "MenuViewController.h"
 
-@interface RootViewController () <TopDelegate, HUDDelegate>
+@interface RootViewController () <TopDelegate, HUDDelegate, UICollisionBehaviorDelegate>
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftPhotosConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *rightPhotosConstraint;
 @property (strong, nonatomic) NSMutableArray *currentTigersArray;
@@ -27,7 +27,6 @@
 @property (nonatomic, strong) UIGravityBehavior *gravityBehavior;
 @property (nonatomic, strong) UIDynamicAnimator *dynamicAnimator;
 @property (nonatomic, strong) UIPushBehavior *pushBehavior;
-@property (weak, nonatomic) IBOutlet UIView *shadeView;
 
 
 
@@ -54,14 +53,44 @@
     self.currentLionsArray = [@[[UIImage imageNamed:@"lion1"],[UIImage imageNamed:@"lion2"],[UIImage imageNamed:@"lion3"]] mutableCopy];
 
 
-
 }
 
--(void)viewDidAppear:(BOOL)animated
+- (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
 
+
+    self.dynamicAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+
+    self.collisionBehavior = [[UICollisionBehavior alloc]initWithItems:@[self.navVC.view]];
+    self.collisionBehavior.collisionDelegate = self;
+
+    self.gravityBehavior = [[UIGravityBehavior alloc]initWithItems:@[self.navVC.view]];
+
+    self.dynamicItemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.navVC.view]];
+
+    self.pushBehavior = [[UIPushBehavior alloc]initWithItems:@[self.navVC.view]
+                                                        mode:UIPushBehaviorModeContinuous];
+
+    [self.collisionBehavior addBoundaryWithIdentifier:@"left"
+                                            fromPoint:CGPointMake(0, -10)
+                                              toPoint:CGPointMake(0, self.view.frame.size.height + 10)];
+
+    [self.collisionBehavior addBoundaryWithIdentifier:@"right"
+                                            fromPoint:CGPointMake(self.view.frame.size.width + self.view.frame.size.width, -10)
+                                              toPoint:CGPointMake(self.view.frame.size.width + self.view.frame.size.width, self.view.frame.size.height + 10)];
+
+    [self.gravityBehavior setGravityDirection:CGVectorMake(0, 0)];
+    [self.dynamicItemBehavior setElasticity:0.1];
+
+    [self.dynamicAnimator addBehavior:self.collisionBehavior];
+    [self.dynamicAnimator addBehavior:self.gravityBehavior];
+    [self.dynamicAnimator addBehavior:self.pushBehavior];
+    [self.dynamicAnimator addBehavior:self.dynamicItemBehavior];
+    
+    
 }
+
 
 #pragma mark - delegate methods implementation
 
@@ -111,30 +140,32 @@
 {
 
     CGPoint translation = [gesture translationInView:gesture.view];
-    self.leftPhotosConstraint.constant = self.leftPhotosConstraint.constant + translation.x;
-    self.rightPhotosConstraint.constant = self.rightPhotosConstraint.constant - translation.x;
-//        gesture.view.center = CGPointMake(gesture.view.center.x + translation.x, gesture.view.center.y);
+//    self.leftPhotosConstraint.constant = self.leftPhotosConstraint.constant + translation.x;
+//    self.rightPhotosConstraint.constant = self.rightPhotosConstraint.constant - translation.x;
+    self.navVC.view.center = CGPointMake(self.navVC.view.center.x + translation.x, self.navVC.view.center.y);
+
+//    self.navVC.view
         //take where it is currently and add or subtract to it based on translation
         //after we used the translation we want to reswt it
     
         [gesture setTranslation:CGPointMake(0, 0) inView:gesture.view];
 
-        CGFloat yVelocity = [gesture velocityInView:gesture.view].x;  // get the x velocity
+        CGFloat xVelocity = [gesture velocityInView:gesture.view].x;  // get the x velocity
         if (gesture.state == UIGestureRecognizerStateEnded)
         {
-            [self.dynamicAnimator updateItemUsingCurrentState:self.shadeView];
+            [self.dynamicAnimator updateItemUsingCurrentState:self.navVC.view];
     
-            if (yVelocity < -500.0) {
+            if (xVelocity < -500.0) {
                 [self.gravityBehavior setGravityDirection:CGVectorMake(-1, 0)];
                 [self.dynamicItemBehavior setElasticity:0.5];
                 [self.pushBehavior setPushDirection:CGVectorMake([gesture velocityInView:gesture.view].x, 0)];
             }
-            else if (yVelocity >= -500.0 && yVelocity < 0) {
+            else if (xVelocity >= -500.0 && xVelocity < 0) {
                 [self.gravityBehavior setGravityDirection:CGVectorMake(-1, 0)];
                 [self.dynamicItemBehavior setElasticity:0.25];
                 [self.pushBehavior setPushDirection:CGVectorMake(-500.0, 0)];
             }
-            else if (yVelocity >= 0 && yVelocity < 500.0) {
+            else if (xVelocity >= 0 && xVelocity < 500.0) {
                 [self.gravityBehavior setGravityDirection:CGVectorMake(1, 0)];
                 [self.dynamicItemBehavior setElasticity:0.25];
                 [self.pushBehavior setPushDirection:CGVectorMake(500.0, 0)];
@@ -144,7 +175,7 @@
                 [self.pushBehavior setPushDirection:CGVectorMake([gesture velocityInView:gesture.view].x, 0)];
             }
         }
-
+[self.view layoutIfNeeded];
 
 }
 
